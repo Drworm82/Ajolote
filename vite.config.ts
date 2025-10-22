3ffc49dbfb5af5d97be5ffa5654c3068a735339f
Drworm82 <worm_jim@hotmail.com>
2025-10-21 21:44:59 -0600

    Add meta tags and manifest link to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
-// vite.config.ts
+<link rel="manifest" href="/manifest.webmanifest" />
+<meta name="theme-color" content="#0f766e" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
