5e7dfa3f18a3b4f2dbb41b6af091b917dd27238c
Drworm82 <worm_jim@hotmail.com>
2025-10-21 22:23:45 -0600

    Refactor routing in main.tsx to use Routes and Route

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import CoinFlip from './pages/coinflip'
+
+// Si usas Pages en subcarpeta, pon el basename para que el router funcione:
+const BASENAME = '/Ajolote'
+
+function App() {
+  return (
+    <BrowserRouter basename={BASENAME}>
+      <Routes>
+        <Route path="/" element={<CoinFlip />} />
+        {/* agrega más rutas aquí si las necesitas */}
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Ajolote">
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 )
